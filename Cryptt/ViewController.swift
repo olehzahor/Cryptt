32eb4a702652edae83b8b3fd977c3bdff411d774
olehzahor <olehzahor@gmail.com>
2022-02-15 11:36:35 +0200

    Initial Commit

---
+++ Cryptt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptt
+//
+//  Created by Oleh Zahoriansky on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
